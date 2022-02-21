80c25780a86f9d54391613189877375f9066e2a0
eccenina <64589290+eccenina@users.noreply.github.com>
2022-02-21 13:27:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,39 +25,41 @@ client.on('ready', () =>{
 
 client.on('messageCreate', async(msg) => {
     if (msg.author.bot) return;
+    const player = createAudioPlayer();
+
     const mapKey = msg.guild.id;
+
+    if (msg.content == "*join"){
+        const connection = joinVoiceChannel({
+            selfDeaf: false,
+            channelId: msg.member.voice.channel.id,
+            guildId: msg.guild.id,
+            adapterCreator: msg.guild.voiceAdapterCreator,
+            
+        });
+        const player = createAudioPlayer();
+        connection.subscribe(player);
+        msg.channel.send('Joined the Vc')        
+        
+    }
     
+    if (msg.content == "*play"){
+        const connection = getVoiceConnection(msg.guild.id)
+        const resource = createAudioResource('./mp3/a.mp3');
+        connection.subscribe(player);
+            player.on( AudioPlayerStatus.Playing, () => {
+                console.log('The audio player has started playing!');
+            });
 
-    switch (msg.content) {
-        case 'b!join':
-            const connection = joinVoiceChannel({
-                selfDeaf: false,
-                channelId: msg.member.voice.channel.id,
-                guildId: mapKey,
-                adapterCreator: msg.guild.voiceAdapterCreator
-            })
-            const player = createAudioPlayer();
-            connection.subscribe(player);
-            msg.channel.send('joined the vc');
-
-        case 'b!play':
-            connection = getVoiceConnection(msg.member.guild.id)
-            connection.subscribe(player);
-            const resource = createAudioResource('./mp3/a.mp3');
-                player.on( AudioPlayerStatus.Playing, () => {
-                    console.log('The audio player has started playing!');
-                });
-
-                player.on('stateChange', (oldState, newState) => {
-                    console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
-                });
-            player.play(resource);
-        case 'b!leave':
-            connection.destroy();
-            console.log('Disconnected');
-
-        
-        
+            player.on('stateChange', (oldState, newState) => {
+                console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
+            });
+        player.play(resource);
+    }
+    if (msg.content == "*leave"){
+        const connection = getVoiceConnection(msg.guild.id)
+        connection.destroy();
+        console.log('Disconnected')
     }
 })
 
